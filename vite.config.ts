f15769143ec69f54f8366bb719cfb737e36dfdd5
Piyush00Soni <Piiyush.sonii@outlook.com>
2025-06-11 22:49:20 +0530

    configure for GitHub Pages 🌐

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MyPortfolio/', // 👈 this is important!
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
